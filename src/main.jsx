625a7c5c9630facf6ac286cbac33ad4725da46cd
Swapvp <swapneshpawar1994@gmail.com>
2024-11-04 14:56:51 +0530

    loader-

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Root from "./routes/root";
+
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
